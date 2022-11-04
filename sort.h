af74115b327d422353bf5b51d1e3f38b5fcd510f
peaceik <igwudup@gmail.com>
2022-11-04 03:06:21 -0700

    sorting

--- sort.h
+++ sort.h
@@ -15,7 +15,11 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void _qksort(int *a, int low, int high, int size);
 #endif
